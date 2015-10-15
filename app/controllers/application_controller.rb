81bb5bffa6bb06095b04dfdfc9fe74cda3f07243
keithconroy <kconroyjr@gmail.com>
2015-10-15 11:51:26 -0700

    add user_id to tweet create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
   def allow_cross_domain
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    # headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
   before_action :allow_cross_domain
   def create
-    @tweet = Tweet.create(body: params[:body])
+    @tweet = Tweet.create(body: params[:body], user_id: params[:user_id])
     if @tweet.save
       render json: @tweet
     else
