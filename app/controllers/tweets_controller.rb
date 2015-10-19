d8b3056c27e52e9d24084fc9c95c484acb103cc7
keithconroy <kconroyjr@gmail.com>
2015-10-18 19:06:35 -0700

    add tweet index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,10 @@
 class TweetsController < ApplicationController
   before_action :allow_cross_domain
+  def index
+    @tweets = Tweet.where(user_id: params[:user_id])
+    render json: @tweets
+  end
+
   def create
     @tweet = Tweet.create(body: params[:body], user_id: params[:user_id])
     if @tweet.save
